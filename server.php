dd6bb2547ea5476426243d46cbf4163128cc1b8b
Dejan Jevtić <dejan_d_jevtic@yahoo.com>
2019-07-02 20:00:41 +0200

    server

--- server.php
+++ server.php
@@ -1,5 +1,17 @@
 <?php
 
-	phpinfo();
-
+	class server{
+		public function __construct(){
+			
+		}
+		
+		public function getStudentName($id_array)
+		{
+			return 'Dejan';
+		}
+	}
+		$params = array('uri'=>'codev/soap/server.php');
+		$server=new SoapServer(NULL, $params);
+		$server->setClass('server');
+		$server->handle();
 ?>
